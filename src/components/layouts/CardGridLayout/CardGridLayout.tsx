bb707998b1051e368de384d1a13e2fca1b34217f
PangHae <yds05074@gmail.com>
2025-09-29 09:26:21 +0900

    style: change stratagy page layout

--- src/components/layouts/CardGridLayout/CardGridLayout.tsx
+++ src/components/layouts/CardGridLayout/CardGridLayout.tsx
@@ -2,7 +2,7 @@ import { FC, PropsWithChildren } from 'react';
 
 const CardGridLayout: FC<PropsWithChildren> = ({ children }) => {
 	return (
-		<div className="grid grid-cols-[repeat(3,420px)] gap-4 place-items-center pb-10">
+		<div className="grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 xxl:grid-cols-4 gap-4 pb-10 place-items-center">
 			{children}
 		</div>
 	);
--- src/containers/strategies/Strategies.client.tsx
+++ src/containers/strategies/Strategies.client.tsx
@@ -12,11 +12,11 @@ const StrategiesClient = () => {
 	};
 
 	return (
-		<div className="flex flex-col gap-8 h-full overflow-y-auto relative">
-			<div className="flex align-center justify-center flex-shrink-0 sticky top-0 bg-custom-gray-bg px-10 pt-20 pb-8">
+		<div className="flex flex-col gap-8 h-full overflow-y-auto relative px-10">
+			<div className="flex align-center justify-center flex-shrink-0 sticky top-0 bg-custom-gray-bg px-10 pt-14 pb-8">
 				<SearchInput onSearch={handleSearch} />
 			</div>
-			<div className="flex flex-col flex-1 min-h-0  mx-auto gap-4">
+			<div className="flex flex-col flex-1 min-h-0 w-full mx-auto gap-4">
 				<div className="flex justify-between px-1">
 					<StrategyStatusFilterMenu />
 					<Button className="bg-shinhan-blue w-[100px] h-[40px] cursor-pointer hover:bg-shinhan-blue/80">
@@ -58,13 +58,13 @@ const StrategiesClient = () => {
 						title="월마트는 무조건 이 전략이다."
 						stock={'월마트(WMT)'}
 						imageUrl={'/icons/WMT.png'}
-						status={'pending'}
+						status={'running'}
 					/>
 					<StrategyCard
 						title="월마트는 무조건 이 전략이다."
 						stock={'월마트(WMT)'}
 						imageUrl={'/icons/WMT.png'}
-						status={'running'}
+						status={'pending'}
 					/>
 				</CardGridLayout>
 			</div>
