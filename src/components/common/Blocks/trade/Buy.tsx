9b3939cc9a53032ba87e7880465a66682cf65196
PangHae <yds05074@gmail.com>
2025-10-08 18:34:12 +0900

    fix: typo

--- src/components/common/Blocks/trade/Buy.tsx
+++ src/components/common/Blocks/trade/Buy.tsx
@@ -6,7 +6,7 @@ const Buy: FC<PropsWithChildren> = ({ children }) => {
 	return (
 		<Block className="flex-1 gap-2 p-4 border-2 border-red-500 rounded-lg bg-red-500/10">
 			<div className="flex items-center gap-2">
-				<Block.title className="text-red-500!">매도</Block.title>
+				<Block.title className="text-red-500!">매수</Block.title>
 				<Block.input
 					className="w-[200px] bg-white! focus-visible:border-red-500"
 					placeholder="몇 주를 매수할까요?"
