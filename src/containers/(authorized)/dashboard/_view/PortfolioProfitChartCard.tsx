5da0c684b14dca65d3ab24e8d1747a6f2fa652f1
PangHae <yds05074@gmail.com>
2025-10-10 10:31:52 +0900

    feat: dashboard > add series filter

--- src/containers/(authorized)/dashboard/_view/PortfolioProfitChartCard.tsx
+++ src/containers/(authorized)/dashboard/_view/PortfolioProfitChartCard.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import { useState } from 'react';
+
 import ProfitRateLineChart from '@/components/charts/ProfitRateLineChart';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { ChartConfig } from '@/components/ui/chart';
@@ -28,12 +30,80 @@ const chartConfig = {
 } satisfies ChartConfig;
 
 const PortfolioProfitChartCard = () => {
+	const [selectedPeriod, setSelectedPeriod] = useState<
+		'oneMonth' | 'threeMonth' | 'sixMonth' | 'oneYear' | 'all'
+	>('oneMonth');
+
 	return (
-		<Card className="flex-1 min-w-0 min-h-0">
+		<Card className="flex-1 min-h-0">
 			<CardHeader>
-				<CardTitle>포트폴리오 수익률</CardTitle>
+				<div className="flex items-start justify-between">
+					<CardTitle>포트폴리오 수익률</CardTitle>
+					<menu className="flex bg-gray-200 rounded-lg p-1">
+						<li>
+							<button
+								className={`px-4 py-2 text-caption font-medium rounded-md transition-all duration-200 ${
+									selectedPeriod === 'oneMonth'
+										? 'bg-white text-black shadow-sm'
+										: 'text-gray-600 hover:text-black'
+								} cursor-pointer`}
+								onClick={() => setSelectedPeriod('oneMonth')}
+							>
+								1M
+							</button>
+						</li>
+						<li>
+							<button
+								className={`px-4 py-2 text-caption  font-medium rounded-md transition-all duration-200 ${
+									selectedPeriod === 'threeMonth'
+										? 'bg-white text-black shadow-sm'
+										: 'text-gray-600 hover:text-black'
+								} cursor-pointer`}
+								onClick={() => setSelectedPeriod('threeMonth')}
+							>
+								3M
+							</button>
+						</li>
+						<li>
+							<button
+								className={`px-4 py-2 text-caption font-medium rounded-md transition-all duration-200 ${
+									selectedPeriod === 'sixMonth'
+										? 'bg-white text-black shadow-sm'
+										: 'text-gray-600 hover:text-black'
+								} cursor-pointer`}
+								onClick={() => setSelectedPeriod('sixMonth')}
+							>
+								6M
+							</button>
+						</li>
+						<li>
+							<button
+								className={`px-4 py-2 text-caption font-medium rounded-md transition-all duration-200 ${
+									selectedPeriod === 'oneYear'
+										? 'bg-white text-black shadow-sm'
+										: 'text-gray-600 hover:text-black'
+								} cursor-pointer`}
+								onClick={() => setSelectedPeriod('oneYear')}
+							>
+								1Y
+							</button>
+						</li>
+						<li>
+							<button
+								className={`px-4 py-2 text-caption font-medium rounded-md transition-all duration-200 ${
+									selectedPeriod === 'all'
+										? 'bg-white text-black shadow-sm'
+										: 'text-gray-600 hover:text-black'
+								} cursor-pointer`}
+								onClick={() => setSelectedPeriod('all')}
+							>
+								All
+							</button>
+						</li>
+					</menu>
+				</div>
 			</CardHeader>
-			<CardContent className="max-h-[calc(100%-40px)] pl-0">
+			<CardContent className="max-h-[calc(100%-72px)] pl-0">
 				<ProfitRateLineChart
 					chartConfig={chartConfig}
 					chartLineData={chartLineData}
