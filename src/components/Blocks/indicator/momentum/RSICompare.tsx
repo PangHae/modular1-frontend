686aed6633e2e60e9c229e4e2ea60e466bc0c9ec
PangHae <yds05074@gmail.com>
2025-10-13 15:14:01 +0900

    feat: add rsi compare block

---
+++ src/components/Blocks/indicator/momentum/RSICompare.tsx
@@ -0,0 +1,131 @@
+'use client';
+
+import { FC, useEffect, useRef, useState } from 'react';
+
+import {
+	BlockProps,
+	ComparisonType,
+	Node,
+	RSIPeriodType,
+	TimeframeType,
+} from '@/@types/StrategyTemplateNode';
+
+import Block from '../../Block';
+
+const RSICompare: FC<BlockProps> = ({ ref }) => {
+	const rightValueRef = useRef<HTMLInputElement>(null);
+
+	const [timeframe, setTimeframe] = useState<TimeframeType>('1d');
+	const [rightValue, setRightValue] = useState<RSIPeriodType>('14');
+	const [rightComparison, setRightComparison] = useState<ComparisonType>('>=');
+
+	const handleChangeTimeframe = (value: string) => {
+		setTimeframe(value as TimeframeType);
+	};
+
+	const handleChangeRightValue = (value: string) => {
+		setRightValue(value as RSIPeriodType);
+	};
+
+	const handleChangeRightComparison = (value: string) => {
+		setRightComparison(value as ComparisonType);
+	};
+
+	const createJson = () => {
+		return {
+			type: 'COMPARE',
+			label: 'rsi_compare',
+			operator: rightComparison,
+			left: {
+				kind: 'INDICATOR',
+				name: 'RSI',
+				args: {
+					period: Number(rightValue),
+				},
+				timeframe,
+			},
+			right: {
+				kind: 'CONSTANT',
+				constant: { value: rightValueRef.current?.value },
+			},
+		} as unknown as Node;
+	};
+
+	useEffect(() => {
+		if (ref?.current) {
+			ref.current.rsiCompare = createJson;
+		} else {
+			ref.current = {
+				rsiCompare: createJson,
+			};
+		}
+	}, [timeframe, rightValue, rightComparison]);
+
+	return (
+		<Block className="flex gap-2 p-4 border-2 border-papaya-orange rounded-lg bg-papaya-orange-bg">
+			<Block.subtitle className="text-papaya-orange">
+				RSI 과매수, 과매도 감지
+			</Block.subtitle>
+			<div className="flex items-center gap-1">
+				<Block.dropdown
+					placeholder="기준"
+					items={[
+						{
+							category: '',
+							options: [
+								{ label: '1분봉', value: '1m' },
+								{ label: '5분봉', value: '5m' },
+								{ label: '15분봉', value: '15m' },
+								{ label: '1시간봉', value: '1h' },
+								{ label: '4시간봉', value: '4h' },
+								{ label: '1일봉', value: '1d' },
+							],
+						},
+					]}
+					value={timeframe}
+					onChange={handleChangeTimeframe}
+				/>
+				기준 RSI가
+				<Block.dropdown
+					placeholder="일자"
+					items={[
+						{
+							category: '',
+							options: [
+								{ label: '7봉', value: '7' },
+								{ label: '14봉', value: '14' },
+								{ label: '21봉', value: '21' },
+							],
+						},
+					]}
+					value={rightValue}
+					onChange={handleChangeRightValue}
+				/>
+				동안
+				<Block.input
+					ref={rightValueRef}
+					type="number"
+					className="w-[100px]"
+					placeholder="값 입력"
+				/>
+				<Block.dropdown
+					placeholder="비교"
+					items={[
+						{
+							category: '',
+							options: [
+								{ label: '이상', value: '>=' },
+								{ label: '이하', value: '<=' },
+							],
+						},
+					]}
+					value={rightComparison}
+					onChange={handleChangeRightComparison}
+				/>
+				일 때
+			</div>
+		</Block>
+	);
+};
+
+export default RSICompare;
